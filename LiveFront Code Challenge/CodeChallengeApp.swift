6c34c14c9e2025e6acd4a1de0ad398d2cc4c9aa3
Jerry Baez <jerry@atomicrobot.com>
2024-11-19 10:31:30 -0600

    Initial commit.

---
+++ LiveFront Code Challenge/CodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeChallengeApp.swift
+//  LiveFront Code Challenge
+//
+//  Created by Jerry Baez on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiveFront Code ChallengeUITests/LiveFront_Code_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LiveFront_Code_ChallengeUITestsLaunchTests.swift
+//  LiveFront Code ChallengeUITests
+//
+//  Created by Jerry Baez on 11/18/24.
+//
+
+import XCTest
+
+final class LiveFront_Code_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
