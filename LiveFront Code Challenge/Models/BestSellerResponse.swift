dcfcd7ad4e4ca2323b973a7eafdd1ababdf6ab21
Jerry Baez <jerry@atomicrobot.com>
2024-11-21 10:43:51 -0600

    Create `BestSellerResponse` object.

---
+++ LiveFront Code Challenge/Models/BestSellerResponse.swift
@@ -0,0 +1,15 @@
+//
+//  BestSellerResponse.swift
+//  LiveFront Code Challenge
+//
+//  Created by Jerry Baez on 11/20/24.
+//
+
+import Foundation
+
+struct BestSellerResponse: Codable {
+
+    // MARK: Constants
+
+    let results: [BestSellerBook]
+}
