2b38de83a2fa85a4c494cb9233c3d1ceb8264dfa
Jerry Baez <jerry@atomicrobot.com>
2024-11-21 10:29:35 -0600

    Create `BestSellerBook` and `BookDetails` model structs.

---
+++ LiveFront Code Challenge/Models/BestSellerBook.swift
@@ -0,0 +1,58 @@
+//
+//  BestSellerBook.swift
+//  LiveFront Code Challenge
+//
+//  Created by Jerry Baez on 11/19/24.
+//
+
+import Foundation
+
+/// Represents a book item on the corresponding best seller list
+struct BestSellerBook: Codable {
+
+    // MARK: Constants
+
+    let listName: String
+    let displayName: String
+    let bestSellersDateString: String
+    let publishedDateString: String
+    let rank: Int
+    let rankLastWeek: Int
+    let weeksOnList: Int
+    let amazonProductUrlString: String
+    let bookDetail: BookDetails
+
+    // MARK: Enums
+
+    enum CodingKeys: String, CodingKey {
+        case listName = "list_name"
+        case displayName = "display_name"
+        case bestSellersDateString = "bestsellers_date"
+        case publishedDateString = "published_date"
+        case rank
+        case rankLastWeek = "rank_last_week"
+        case weeksOnList = "weeks_on_list"
+        case amazonProductUrlString = "amazon_product_url"
+        case bookDetail = "book_details"
+    }
+
+    // mark: Computed Vars
+
+    /// A date object that represents when this book became a best seller
+    var bestSellersDate: Date? { self.getDateFromString(self.bestSellersDateString) }
+
+    /// An URL object where this book can be seen in Amazon.com for purchase
+    var amazonProductUrl: URL? { URL(string: self.amazonProductUrlString) }
+
+    // MARK: Utility Methods
+
+    /// Returns a Date object based on the given string (NOTE: The string format must be "yyyy-MM-dd" for this method to return a valid Date object)
+    /// - Parameter dateString: The string representing a data in "yyyy-MM-dd" format
+    /// - Returns: A Data object representing the given "yyyy-MM-dd" formatted string
+    private func getDateFromString(_ dateString: String) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        return dateFormatter.date(from: dateString)
+    }
+}
---
+++ LiveFront Code Challenge/Models/BookDetails.swift
@@ -0,0 +1,40 @@
+//
+//  BookDetails.swift
+//  LiveFront Code Challenge
+//
+//  Created by Jerry Baez on 11/19/24.
+//
+
+import Foundation
+
+/// Represents the details of a book in a best seller list
+struct BookDetails: Codable {
+
+    // MARK: Constants
+
+    let title: String
+    let description: String
+    let contributor: String
+    let author: String
+    let contributorNote: String
+    let price: String
+    let ageGroup: String
+    let publisher: String
+    let primaryIsbn13: String
+    let primaryIsbn10: String
+
+    // MARK: Enums
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description
+        case contributor
+        case author
+        case contributorNote
+        case price
+        case ageGroup = "age_group"
+        case publisher
+        case primaryIsbn13 = "primary_isbn13"
+        case primaryIsbn10 = "primary_isbn10"
+    }
+}
